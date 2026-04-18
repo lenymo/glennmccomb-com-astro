efc396e6012876507c993a046e805f678fc4753d
Glenn McComb <lenymo@gmail.com>
2026-04-19 07:52:56 +1000

    more article scaffolding; adds rust article

---
+++ src/data/socialLinks.ts
@@ -0,0 +1,33 @@
+export interface SocialLink {
+  identifier: string;
+  name: string;
+  url: string;
+}
+
+export const socialLinks: SocialLink[] = [
+  {
+    identifier: "twitter",
+    name: "Twitter",
+    url: "https://twitter.com/lenymo",
+  },
+  {
+    identifier: "instagram",
+    name: "Instagram",
+    url: "https://www.instagram.com/lenymo",
+  },
+  {
+    identifier: "github",
+    name: "GitHub",
+    url: "https://github.com/lenymo",
+  },
+  {
+    identifier: "linkedin",
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/glennmccomb",
+  },
+  {
+    identifier: "dribbble",
+    name: "Dribbble",
+    url: "https://dribbble.com/lenymo",
+  },
+];
